e5be3171c4cf116a76d97c79fdcf6c4d1fd56708
CoAuToSe <11648807+CoAuToSe@users.noreply.github.com>
2021-12-28 19:44:22 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! (WIP)");
+    // need to get the env for the folder "target"
+    // need to clean partialy the target dir with default value being all level available (debug, release, ..)
+    //// possibilities to choose the level to clear
+    // then run the "build" command
 }
