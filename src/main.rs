67d3a381b0216326540a244672070f2beb7e9286
CoAuToSe <11648807+CoAuToSe@users.noreply.github.com>
2022-02-11 20:54:56 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
+// use std::{
+//     env::{self, *},
+//     path::{self, *},
+// };
+
+// fn main() {
+//     println!("Hello, world! (WIP)");
+//     // need to get the env for the folder "target"
+//     // need to clean partialy the target dir with default value being all level available (debug, release, ..)
+//     //// possibilities to choose the level to clear
+//     // then run the "build" command
+// }
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opts {
+    #[structopt(parse(from_os_str))]
+    infile: PathBuf,
+
+    #[structopt(short, long, parse(from_os_str))]
+    outfile: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world! (WIP)");
-    // need to get the env for the folder "target"
-    // need to clean partialy the target dir with default value being all level available (debug, release, ..)
-    //// possibilities to choose the level to clear
-    // then run the "build" command
+    let opts = Opts::from_args();
+
+    println!("{:?}", opts);
 }
